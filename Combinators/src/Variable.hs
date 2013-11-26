b5fca039d477f4fadecf931dd3567e3d783f15b6
Jürgen jutaro Nicklisch-Franken <jnf@arcor.de>
2013-11-26 17:57:19 +0100

    Added module Variable

---
+++ Combinators/src/Variable.hs
@@ -0,0 +1,55 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+-----------------------------------------------------------------------------
+--
+-- Module      :  Variable
+-- Copyright   :  All rights reserved, Jürgen Nicklisch-Franken
+-- License     :  GPL (Just (Version {versionBranch = [2], versionTags = []}))
+--
+-- Maintainer  :  Jürgen Nicklisch-Franken
+-- Stability   :  provisional
+-- Portability :
+--
+-- |
+--
+-----------------------------------------------------------------------------
+
+module Variable where
+
+import Test.QuickCheck (elements, Arbitrary(..))
+import Text.Parsec.String (Parser)
+import qualified Text.Parsec as PA ((<?>), noneOf, many, lower,spaces)
+
+-- | A variable can be
+--
+-- * printed
+-- * parsed
+-- * instantiated from a string representation
+--
+-- * compared for equality
+
+class (Show v, Eq v) => Variable v where
+    varPp     :: v -> String
+        -- ^ needs to start with lower case character
+    varParse  :: Parser v
+    varString :: VarString -> v -- Fixme with state
+    varGen    :: Int -> [v]
+
+-- | The representation of variables as strings
+type VarString = String
+
+instance Variable VarString where
+    varPp = id
+    varParse = do
+            PA.spaces
+            start <- PA.lower
+            rest <- PA.many (PA.noneOf " ()\t\n\r\f\v.")
+            return (start:rest)
+        PA.<?> "varParse for SimpleVar"
+    varString = id
+    varGen i = map (\i' -> "v_" ++ show i') [1 .. i]
+
+instance Arbitrary VarString where
+    arbitrary = elements ["u","v","w","x","y","z"]
+
+
